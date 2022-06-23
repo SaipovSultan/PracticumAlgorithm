6bb97fa347e2dcb9ec8b6c2b42c3c924e612f0ec
Sultan Saipov <saipov-1995@mail.ru>
2022-06-23 13:09:05 +0300

    Спринт 6, A. Построить список смежности.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
 #include "streamcollection.h"
 #include <iostream>
 #include <vector>
+#include <forward_list>
 using namespace std;
 
 vector<vector<bool>> create_adjacency_matrix(const vector<pair<size_t, size_t>>& edges) {
-    size_t n = 0u;
+    vector<vector<bool>> adjacency_matrix;
     for(const auto& [from, to] : edges) {
-        n = max(n, max(from, to));
-    }
-    vector<vector<bool>> adjacency_matrix(n + 1u, vector<bool>(n + 1u, false));
-    for(const auto& [from, to] : edges) {
+        if(adjacency_matrix.size() <= from) {
+            adjacency_matrix.resize(from + 1u);
+        }
+        if(adjacency_matrix[from].size() <= to) {
+            adjacency_matrix[from].resize(to + 1u);
+        }
         adjacency_matrix[from][to] = true;
     }
     return adjacency_matrix;
+}
+
+vector<forward_list<int>> create_adjacency_list(const vector<pair<size_t, size_t>>& edges) {
+    vector<forward_list<int>>  adjacency_list;
+    for(const auto& [from, to] : edges) {
+        if(adjacency_list.size() <= from) {
+            adjacency_list.resize(from + 1u);
+        }
+        adjacency_list[from].push_front(to);
+    }
+    return adjacency_list;
 }
 
 int main() {
@@ -21,14 +35,7 @@ int main() {
     while(cin >> from && cin >> to) {
         edges.push_back(make_pair(from, to));
     }
-    vector<vector<bool>> adjacency_matrix = create_adjacency_matrix(edges);
-    for(const auto& edges : adjacency_matrix) {
-        cout << count(cbegin(edges), cend(edges), true);
-        for(size_t i = 0u;i < edges.size();++i) {
-            if(edges[i]) cout << " " << i;
-        }
-        cout << "\n";
-    }
-    cout << adjacency_matrix << endl;
+    auto adjacency_matrix = create_adjacency_matrix(edges);
+    auto adjacency_list = create_adjacency_list(edges);
     return 0;
 }
